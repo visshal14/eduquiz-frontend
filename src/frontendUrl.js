0d9365728762285f67759f2b3039e34a3d982d44
visshal14 <vishalpal2912@gmail.com>
2024-04-10 15:52:27 +0530

    Update frontendUrl.js

--- src/frontendUrl.js
+++ src/frontendUrl.js
@@ -1,20 +1,20 @@
 //for localhost
-const url = "http://localhost:3000"
+// const url = "http://localhost:3000"
 // const backendUrl = "http://localhost:4000"
-const peerPort = "4000"
-const peerSecure = false
+// const peerPort = "4000"
+// const peerSecure = true
 
 
 //for server
 
-// const url = "http://localhost:3000"
-// const backendUrl = "http://localhost:4000"
-// const peerPort = "443"
-// const peerSecure = true
-
-
-
+const url = "https://eduquiz01.netlify.app"
 const backendUrl = "https://eduquiz001.onrender.com"
+const peerPort = "443"
+const peerSecure = true
+
+
+
+
 
 
 export default url
